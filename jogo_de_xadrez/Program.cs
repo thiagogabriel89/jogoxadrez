1d7d75ae2bedc7c183e3db9eea9dae5cab603486
thiagogabriel89 <ts.gabriel89@gmail.com>
2022-04-26 16:00:10 -0300

    inicio_do_projeto

---
+++ jogo_de_xadrez/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogo_de_xadrez
+{
+    internal class Program
+    {
+        class program
+        {
+            static void Main(string[] args) { }
+
+        }
+    }
+}
